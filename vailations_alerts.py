6d2d9ce05cd7740951344c6f8812262ca0a7addf
Samruddha Honawade <131781660+SamruddhaHonawade@users.noreply.github.com>
2024-06-15 13:37:44 +0530

    Update vailations_alerts.py

--- vailations_alerts.py
+++ vailations_alerts.py
@@ -10,29 +10,27 @@ import getpass
 import csv
 import cv2
 import threading
-
 import random
-
-# Load environment variables from .env file
 load_dotenv()
 
-# Retrieve Supabase URL and Key from environment variables
+
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 
-# Create a Supabase client
-supabase = create_client(url, key)
-role='Developer'
 
-# Define the allowed applications for each role
+supabase = create_client(url, key)
+#role='Developer'
+role='Marketing'
+
+
 role_app_access = {
-    "Developer": ["pycharm64.exe", "python.exe", "code.exe", "sublime_text.exe", "idea64.exe", "eclipse.exe"],
+    "Developer": ["pycharm64.exe", "python.exe", "code.exe", "sublime_text.exe", "idea64.exe", "eclipse.exe","ApplicationFrameHost.exe"],
     "Designer": ["photoshop.exe", "illustrator.exe", "sketch.exe", "figma.exe"],
     "Finance": ["excel.exe", "sheets.exe", "quickbooks.exe"],
-    # Add more roles and their allowed applications as needed
+    "Marketing": ["photoshop.exe", "illustrator.exe", "figma.exe", "chrome.exe","explorer.exe"],
 }
 
-# Define the maximum allowed access count for each application
+
 app_access_limit = {
     "pycharm64.exe": 20,
     "code.exe": 20,
@@ -41,15 +39,22 @@ app_access_limit = {
     "ApplicationFrameHost.exe":5,
     "eclipse.exe": 10,
     "settings.exe": 5,
-    # Add more applications and their access limits as needed
+    "SearchHost.exe": 3,
+    "RuntimeBroker.exe": 3,
+    "MicrosoftEdge.exe": 5,
+    "chrome.exe": 5,
+    "Notepad.exe":2,
+
+
+
 }
 
-# Define a dictionary to keep track of violations
+
 violations = {}
 user_name = getpass.getuser()
 
 def capture_photo():
-    # Initialize webcam
+
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
@@ -58,7 +63,7 @@ def capture_photo():
 
     try:
         while True:
-            # Read and display frame from webcam
+
             ret, frame = cap.read()
             cv2.imshow("Press 's' to save or 'q' to quit", frame)
 
@@ -83,36 +88,36 @@ def capture_photo():
         fileimg_name = "user1" + str(r_num) + ".png"
         data = supabase.storage.from_(bucket_name).upload(fileimg_name, new_file)
         print(data)
-# Function to get the active window's application name
+
 def get_active_window():
     try:
-        hwnd = win32gui.GetForegroundWindow()  # Get handle to the foreground window
-        pid = win32process.GetWindowThreadProcessId(hwnd)  # Get PID of the process
-        process = psutil.Process(pid[-1])  # Get process info
-        app_name = process.name()  # Get the process name
+        hwnd = win32gui.GetForegroundWindow()
+        pid = win32process.GetWindowThreadProcessId(hwnd)
+        process = psutil.Process(pid[-1])
+        app_name = process.name()
         return app_name
     except Exception as e:
         return None
 
-# Function to log violations to Supabase
+
 def log_violations(app, duration):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S (%A)")  # Format timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S (%A)")
     count = violations[app]
     supabase.table("violations").insert(
         {"user_name": user_name, "application": app, "violation_count": count, "timestamp": timestamp, "role": role, "duration": duration}
     ).execute()
 
-# Function to log application usage
+
 def log_application_usage():
     last_app = None
-    app_access_count = {}  # Dictionary to keep track of application access count
+    app_access_count = {}
     start_time = None
-    no_action_start_time = None  # Variable to keep track of the start time of no action
+    no_action_start_time = None
 
     csv_file = "application_usage_log.csv"
-    user_name = getpass.getuser()  # Get the current username
+    user_name = getpass.getuser()
 
-    # Check if the CSV file exists to write the header only once
+
     file_exists = os.path.isfile(csv_file)
 
     with open(csv_file, mode='a', newline='') as file:
@@ -142,10 +147,10 @@ def log_application_usage():
                     else:
                         app_access_count[current_app] = 1
 
-                    # Debug: Print the access count and limit for the current application
+
                     print(f"Debug: {current_app} - Access Count: {app_access_count[current_app]}, Access Limit: {app_access_limit.get(current_app, 'No limit set')}")
 
-                    # Print an alert if the access count for the current application exceeds its maximum allowed count
+
                     if current_app in app_access_limit and app_access_count[current_app] > app_access_limit[current_app]:
                         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get the current time
                         # code to Db
@@ -156,33 +161,33 @@ def log_application_usage():
                         capture_photo()
                         print(f"Alert: The application {current_app} has been accessed more than {app_access_limit[current_app]} times at {current_time}!")
 
-                    # Check if the current application is allowed for the user's role
+
                     user_role = supabase.table("user_analysis").select("role").filter("user_name", "eq", user_name).execute().data[0]["role"]
                     if user_role not in role_app_access or current_app not in role_app_access[user_role]:
-                        # If the application is not allowed, add it to the violations dictionary
+
                         if current_app in violations:
                             violations[current_app] += 1
                         else:
                             violations[current_app] = 1
                         print(f"Violation: {user_name} accessed {current_app} which is not allowed for their role {user_role}. Total violations for this app: {violations[current_app]}")
                         log_violations(current_app, duration_minutes)
-                    # Only consider the applications accessed in the last 3 hours
+
                     three_hours_ago = time.time() - 3 * 60 * 60
                     if start_time >= three_hours_ago:
                         writer.writerow([timestamp, current_app, user_name, duration_minutes, app_access_count[current_app]])
-                        # Adding data to Supabase
+
                         supabase.table("user_analysis").insert(
                             {"log_time": timestamp, "application": current_app, "user_name": user_name, "duration": duration_minutes, "access_count": app_access_count[current_app],"role":role}
                         ).execute()
                         print(timestamp, current_app, user_name, f"{duration_minutes} minutes", f"Access Count: {app_access_count[current_app]}")
                     last_app = current_app
-                    no_action_start_time = None  # Reset the no action start time when an application is active
+                    no_action_start_time = None
             else:
-                # If no application is active, start the no action timer
+
                 if no_action_start_time is None:
                     no_action_start_time = current_time
                 else:
-                    # If no application has been active for 1 minute, print an alert
+
                     if current_time - no_action_start_time >= 60:
                         print("Alert: No process has been in action for 1 minute!")
             time.sleep(1)  # Check every second
