ef93fd1c9d8ecab171927e3a62100f9e7c98de3a
Samruddha Honawade <131781660+SamruddhaHonawade@users.noreply.github.com>
2024-06-15 13:11:51 +0530

    Update vailations_alerts.py

--- vailations_alerts.py
+++ vailations_alerts.py
@@ -8,7 +8,10 @@ import psutil
 import time
 import getpass
 import csv
+import cv2
 import threading
+
+import random
 
 # Load environment variables from .env file
 load_dotenv()
@@ -23,7 +26,7 @@ role='Developer'
 
 # Define the allowed applications for each role
 role_app_access = {
-    "Developer": ["pycharm64.exe", "code.exe", "sublime_text.exe", "idea64.exe", "eclipse.exe"],
+    "Developer": ["pycharm64.exe", "python.exe", "code.exe", "sublime_text.exe", "idea64.exe", "eclipse.exe"],
     "Designer": ["photoshop.exe", "illustrator.exe", "sketch.exe", "figma.exe"],
     "Finance": ["excel.exe", "sheets.exe", "quickbooks.exe"],
     # Add more roles and their allowed applications as needed
@@ -45,6 +48,41 @@ app_access_limit = {
 violations = {}
 user_name = getpass.getuser()
 
+def capture_photo():
+    # Initialize webcam
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+
+    try:
+        while True:
+            # Read and display frame from webcam
+            ret, frame = cap.read()
+            cv2.imshow("Press 's' to save or 'q' to quit", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('s'):  # Press 's' to save the photo
+                timestamp = time.strftime("%Y%m%d_%H%M%S")
+                filename = f"captured_photo_{timestamp}.jpg"
+                cv2.imwrite(filename, frame)
+                print(f"Photo saved as {filename}")
+
+            elif key == ord('q'):
+                break
+
+            time.sleep(1)
+
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+        bucket_name: str = "Samruddha"
+        new_file = filename
+        r_num = random.randint(1, 10)
+        fileimg_name = "user1" + str(r_num) + ".png"
+        data = supabase.storage.from_(bucket_name).upload(fileimg_name, new_file)
+        print(data)
 # Function to get the active window's application name
 def get_active_window():
     try:
@@ -113,9 +151,9 @@ def log_application_usage():
                         # code to Db
 
                         supabase.table("Alerts").insert(
-                            {"alert": f'The application {current_app} has been accessed more than {app_access_limit[current_app]} Times'}
+                            {"alert": f'The application {current_app} has been accessed more than {app_access_limit[current_app]} Times used by {user_name}', "user_name":user_name}
                         ).execute()
-
+                        capture_photo()
                         print(f"Alert: The application {current_app} has been accessed more than {app_access_limit[current_app]} times at {current_time}!")
 
                     # Check if the current application is allowed for the user's role
